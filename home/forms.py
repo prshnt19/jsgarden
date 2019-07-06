d43f383287f0d3692f632a24d3552af6c3d4af49
Abhishek <uchiha@pop-os.localdomain>
2019-07-06 11:39:48 +0530

    gallery and enquiry form

--- home/forms.py
+++ home/forms.py
@@ -3,4 +3,8 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     contact = forms.IntegerField()
-    message = forms.CharField(widget=forms.Textarea)
+    no_of_guests = forms.IntegerField()
+    eventdate = forms.CharField()
+    email=forms.EmailField(required=False)
+    package = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea,required=False)
--- home/views.py
+++ home/views.py
@@ -14,10 +14,22 @@ def index(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             contact = form.cleaned_data['contact']
+            no_of_guests = form.cleaned_data['no_of_guests']
+            date = form.cleaned_data['eventdate']
+            email =  form.cleaned_data['email']
+            package= form.cleaned_data['package']
             message = form.cleaned_data['message']
 
+
+            # date= "-".join(date)
             contact = str(contact)
-            final="Name-"+name+"\n"+"Contact Number-"+contact+"\n"+"Message-"+message
+            no_of_guests = str(no_of_guests)
+            final="Name-"+name+"\n"+"Contact Number-"+contact+"\n"
+            final = final + 'Email-'+email + '\n'
+            final = final + 'Package-'+ package+'\n'+'No. of Guests-'+no_of_guests + '\n'+'Date:-'+date + '\n'
+            final = final +'Message-' + message
+
+            # print(final)
 
             client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
             response = client.messages.create(
